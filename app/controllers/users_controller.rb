4677fa1a40ef01c8ca2ac4e0cf64b3b36ea2bd7a
Alex Krueger <alexckrueger@gmail.com>
2022-05-16 20:54:23 -0500

    adds user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def create
+    # User create
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.name = params[:name] || user.name
+    if user.save
+      render json: user
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {message: "YOU has been obliterated to infinity and beyond and back once again"}
+  end
 end
